ab219fc7628d567a4216f76f99f06bc41a9e16ab
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-08-09 02:06:19 +0900

    add: news api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
+import axios from "../node_modules/axios/index";
 
 function App() {
-  return <div className="App"></div>;
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        "https://newsapi.org/v2/top-headlines?country=kr&apiKey=cce2c301975a4fb3b9303ab626583b39"
+      );
+
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
+    </div>
+  );
 }
 
 export default App;
