750fbd972d02f1ca95901b4faa1dad1e8bf3a385
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-08-10 00:56:35 +0900

    Add: NavLink react-router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import React, { useCallback, useState } from "react";
-import NewsList from "./components/NewsList";
-import Categories from "./components/Categories";
-
+import React from "react";
+import NewsPage from "./pages/NewsPage";
+import { Route } from "react-router-dom";
 function App() {
-  const [category, setCategory] = useState("all");
-  const onSelect = useCallback((category) => setCategory(category), []);
-
-  return (
-    <>
-      <Categories category={category} onSelect={onSelect} />
-      <NewsList category={category} />;
-    </>
-  );
+  return <Route path="/:category?" component={NewsPage} />;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
+import { BrowserRouter } from "../node_modules/react-router-dom/index";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 );
