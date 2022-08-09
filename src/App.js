cffbfb246163a3460e0e7600992a0f77f09f551c
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-08-09 12:32:17 +0900

    TEST NewsList

--- src/App.js
+++ src/App.js
@@ -1,34 +1,8 @@
-import "./App.css";
-import { useState } from "react";
-import axios from "../node_modules/axios/index";
+import React from "react";
+import NewsList from "./components/NewsList";
 
 function App() {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        "https://newsapi.org/v2/top-headlines?country=kr&apiKey=cce2c301975a4fb3b9303ab626583b39"
-      );
-
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  );
+  return <NewsList />;
 }
 
 export default App;
